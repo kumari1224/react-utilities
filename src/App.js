18afac5c5acbbc1840a5fdbe2a00c1ab32285fb8
Priyanka Kumari <priyankakumari@gomechanic.in>
2022-07-19 19:53:22 +0530

    Added first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Loadable from 'react-loadable';
 
-const Loading = ()=><div><center><img  src={ require('./images/avatar_black.gif') } alt="loading" height="100" weight="100" /></center></div>
-
-const RootTableContainer = Loadable({
-  loader: () => import('./containers/RootTableContainer'),
-  loading: Loading
-});
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={RootTableContainer}/>
-        </Switch>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware} from 'redux';
-import { Provider } from 'react-redux'
-import rootReducer from './reducers'
-import * as serviceWorker from './serviceWorker';
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-)
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-serviceWorker.unregister();
